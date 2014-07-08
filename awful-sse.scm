6edd64bc5d41fd2a51fb0b131b64cbdb1e004a24
arthurmaciel <arthurmaciel@gmail.com>
2014-07-07 23:13:58 -0300

    Updated copyrights

--- awful-sse.scm
+++ awful-sse.scm
@@ -1,4 +1,4 @@
-;; Copyright (c) 2010-2014, Mario Domenech Goulart
+;; Copyright (c) 2010-2014, Arthur Maciel
 ;; All rights reserved.
 ;;
 ;; Redistribution and use in source and binary forms, with or without
