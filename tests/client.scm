60e14a84038f5b28acd32f7eb55cad64d8b99b95
arthurmaciel <arthurmaciel@gmail.com>
2014-07-08 15:45:06 -0300

    Added tests

---
+++ tests/client.scm
@@ -0,0 +1,20 @@
+(use awful-sse)
+
+(define (sse-proc)
+  (send-sse-data "sse"))
+
+(define-page/sse "/client"
+  (lambda ()
+    ;; Unnecessary as our client it not a browser
+    ;; (add-javascript 
+    ;;  "var source = new EventSource('/sse');
+    ;;   source.onmessage = function (event) {
+    ;;       display = document.getElementById('display');
+    ;;       display.innerHTML = event.data;
+    ;;   };")
+
+    "foo") ; page contents won't be accessed anyway
+
+  "/sse"
+  sse-proc
+  no-template: #t)
---
+++ tests/run.scm
@@ -0,0 +1,17 @@
+(use awful http-client intarweb uri-common server-test test)
+
+(awful-apps (list "client.scm"))
+
+(with-test-server
+ (lambda ()
+   (awful-start
+    (lambda ()
+      (load-apps (awful-apps)))))
+ (lambda ()   
+   (test "data: sse\n\n"
+         (with-input-from-request
+	  (make-request
+	   uri: (uri-reference "http://localhost:8080/sse")
+	   headers: (headers '((accept text/event-stream))))
+	  #f
+	  read-string))))
