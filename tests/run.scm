b8cab6e3b5a387c18725a805351911f8ab276409
arthurmaciel <arthurmaciel@gmail.com>
2014-07-09 15:37:21 -0300

    Added (test-exit) at the end of tests/run.scm

--- tests/run.scm
+++ tests/run.scm
@@ -9,9 +9,11 @@
       (load-apps (awful-apps)))))
  (lambda ()   
    (test "data: sse\n\n"
-         (with-input-from-request
+	 (with-input-from-request
 	  (make-request
 	   uri: (uri-reference "http://localhost:8080/sse")
 	   headers: (headers '((accept text/event-stream))))
 	  #f
 	  read-string))))
+
+(test-exit)
